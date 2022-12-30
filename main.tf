ae64b3c848619bd1c308c61ddcfbc4bff6528980
Sharifoff <efqanserifov@gmail.com>
2022-12-30 10:57:12 +0000

    komment

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "bakkit3112220222"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
